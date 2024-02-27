51444ac084c470da8676f2a1ec55e11326e274da
mmiller1234321 <148365315+mmiller1234321@users.noreply.github.com>
2024-02-27 14:40:46 -0600

    updated css and test

--- index.js
+++ index.js
@@ -1,26 +1,69 @@
-
-// index.js
-
-const { promptUser } = require('./Assets/lib/userInput.js');
-const { generateSVG } = require('./Assets/lib/svgGenerator.js');
+const Triangle = require('./Assets/lib/shapes').Triangle;
+const Square = require('./Assets/lib/shapes').Square;
+const Circle = require('./Assets/lib/shapes').Circle;
+const inquirer = require('inquirer');
 const fs = require('fs');
+//inquire prompts for logo, text color, and shape
 
-async function main() {
-  try {
-    const userInput = await promptUser();
-    const { text, textColor, shape, shapeColor } = userInput;
+inquirer.prompt ([{
 
-    // Generate SVG
-    const svgContent = generateSVG(text, textColor, shape, shapeColor);
+  type: 'input',
+  message: 'Enter 3 characters for the logo',
+  name: 'logo',
 
-    // Write SVG content to file
-    fs.writeFileSync('./examples/logo.svg', svgContent);
-
-    console.log('Generated logo.svg');
-    console.log('Open logo.svg in a browser to view the logo as an image.');
-  } catch (error) {
-    console.error('An error occurred:', error);
+  validate:function(input){
+    if(input.length === 3){
+      return true;
+    }
+    else{
+      return 'Please enter 3 characters';
+    }
   }
-}
 
-main();
+}, // Added a comma here
+
+{
+  type:'input',
+  message: 'Enter text color for logo',
+  name: 'textColor',
+}, // Added a comma here
+
+{
+  type: `input`,
+  message: `Enter the shape's color for your logo`,
+  name: `shapeColor`,
+},
+]).then(function(answers){
+  if(answers.shape === 'triangle'){
+    let triangle = new Triangle(answers.logo, answers.textColor, answers.shapeColor);
+    fs.writeFile('logo.svg', triangle.render(), function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log('Generated lovo.svg');
+      }
+    });
+  } else if (answers.shape === 'square'){
+    let square = new Square(answers.logo, answers.textColor, answers.shapeColor);
+    fs.writeFile('logo.svg', square.render(), function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log('Generated lovo.svg');
+      }
+    });
+  } else if (answers.shape === 'circle'){
+    let circle = new Circle(answers.logo, answers.textColor, answers.shapeColor);
+    fs.writeFile('logo.svg', circle.render(), function(err){
+      if(err){
+        console.log(err);
+      }
+      else{
+        console.log('Generated lovo.svg');
+      }
+    });
+  }
+
+})
